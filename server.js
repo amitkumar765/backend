5d0c542d5398bc21a1be64b61a2a4666bb86a959
amitkumar765 <117889369+amitkumar765@users.noreply.github.com>
2025-03-29 14:43:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ const errorHandler = require("./middleware/errorMiddleware");
 const app = express();
 
 // Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(bodyParser.json());
 app.use(
   cors({
     origin: ["https://deft-faun-4417db.netlify.app/", "https://frontend-sepia-mu-42.vercel.app/"],
@@ -21,6 +17,11 @@ app.use(
   })
 );
 app.options("*", cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+
 
 
 // Routes
